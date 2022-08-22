9d01dca40e68ea68b83575d994d988dbfc3a4713
koka_a <malakalsayaghi@gmail.com>
2022-08-22 16:28:33 +0000

    quotes and quotes item done

---
+++ app/src/main/java/com/goread/library/activities/QuotesActivity.java
@@ -0,0 +1,16 @@
+package com.goread.library.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.goread.library.R;
+
+public class QuotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes);
+    }
+}
