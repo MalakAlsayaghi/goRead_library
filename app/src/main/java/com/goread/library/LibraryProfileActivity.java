8e3ddb3a2e3f6720c60708112c3b68f28b2301ed
koka_a <malakalsayaghi@gmail.com>
2022-08-28 15:38:10 +0000

    add library profile

--- app/src/main/java/com/goread/library/LibraryProfileActivity.java
+++ app/src/main/java/com/goread/library/LibraryProfileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfilActivity extends AppCompatActivity {
+public class LibraryProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_admin);
+        setContentView(R.layout.activity_library_profile);
     }
 }
