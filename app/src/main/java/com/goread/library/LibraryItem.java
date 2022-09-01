409f2333870b294f1b33fc1485c725a7acd77402
koka_a <malakalsayaghi@gmail.com>
2022-09-01 16:03:44 +0000

    add edit button to (driver,order,library)

---
+++ app/src/main/java/com/goread/library/LibraryItem.java
@@ -0,0 +1,38 @@
+package com.goread.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class LibraryItem extends AppCompatActivity {
+ImageButton edit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library_item);
+
+        edit =findViewById(R.id.edit_item);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(LibraryItem.this, edit);
+                popupMenu.getMenuInflater().inflate(R.menu.popupmenu_edit, popupMenu.getMenu());
+                /////
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(LibraryItem.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
+            }
+        });
+    }}
--- app/src/main/java/com/goread/library/fragments/QoutesFragment.java
+++ app/src/main/java/com/goread/library/fragments/QoutesFragment.java
@@ -61,6 +61,6 @@ public class QoutesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qoutes, container, false);
+        return inflater.inflate(R.layout.fragment_quotes, container, false);
     }
 }
