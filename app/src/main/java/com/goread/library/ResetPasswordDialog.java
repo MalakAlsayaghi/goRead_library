7eec8458723cf57cb1d6bfd1beff5d277b0684e2
koka_a <malakalsayaghi@gmail.com>
2022-09-16 20:52:42 +0000

    add reset password dialog

---
+++ app/src/main/java/com/goread/library/ResetPasswordDialog.java
@@ -0,0 +1,14 @@
+package com.goread.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPasswordDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_password_dialog);
+    }
+}
