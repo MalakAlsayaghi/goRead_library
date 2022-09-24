9a2150511250d608b0b2ba42262e5d6b42838234
saba <saba.albashiri@gmail.com>
2022-09-24 13:22:54 +0300

    add-notification is now connected  to fb2

--- app/src/main/java/com/goread/library/admin/activities/AddNotificationActivity.java
+++ app/src/main/java/com/goread/library/admin/activities/AddNotificationActivity.java
@@ -17,38 +17,39 @@ import com.google.firebase.database.DatabaseReference;
 import com.goread.library.R;
 
 public class AddNotificationActivity extends AppCompatActivity {
-EditText title;
-EditText body;
-Button buttonaAddNotification;
-DatabaseReference dbnotification;
+    EditText title;
+    EditText body;
+    Button buttonaAddNotification;
+    DatabaseReference dbnotification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_notification);
         dbnotification = FirebaseDatabase.getInstance().getReference("Notification");
-        title=(EditText) findViewById(R.id.titleNotification);
-        body= (EditText) findViewById(R.id.bodyNotification);
+        title = (EditText) findViewById(R.id.titleNotification);
+        body = (EditText) findViewById(R.id.bodyNotification);
         buttonaAddNotification = (Button) findViewById(R.id.btn_add_Notification);
         buttonaAddNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-addNotification();
+                addNotification();
             }
         });
 
-        }
-    private void addNotification(){
+    }
+
+    private void addNotification() {
         String theTitle = title.getText().toString().trim();
         String theBody = body.getText().toString().trim();
-        if(!TextUtils.isEmpty(theTitle)){
-            String id= dbnotification.push().getKey();
-            Notification notification=new Notification(id,theTitle,theBody);
+        if (!TextUtils.isEmpty(theTitle)) {
+            String id = dbnotification.push().getKey();
+            Notification notification = new Notification(id, theTitle, theBody);
             dbnotification.child(id).setValue(notification);
 
             Toast.makeText(this, "notification added", Toast.LENGTH_LONG).show();
-        }
-        else
-            Toast.makeText(this,"you have to enter a title and a text",Toast.LENGTH_LONG).show();
+        } else
+            Toast.makeText(this, "you have to enter a title and a text", Toast.LENGTH_LONG).show();
     }
 
 }
