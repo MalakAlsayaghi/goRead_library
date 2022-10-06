0647cc4ca7cf50fce6a6dc7c5d75471c0a9706fb
Mohammed <alhaddad.moha@gmail.com>
2022-10-06 13:31:57 +0300

    show admins+shimmer

--- app/src/main/java/com/goread/library/admin/activities/AllAdminsActivity.java
+++ app/src/main/java/com/goread/library/admin/activities/AllAdminsActivity.java
@@ -1,30 +1,139 @@
 package com.goread.library.admin.activities;
 
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.goread.library.R;
+import com.goread.library.admin.adapters.AdminAdapter;
+import com.goread.library.models.User;
 
-public class AllAdminsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllAdminsActivity extends AppCompatActivity implements AdminAdapter.AdapterCallback {
     ImageView back_btn;
     ImageView add_btn;
     RecyclerView allAdmins_recyclerView;
+    AdminAdapter adminAdapter;
+    List<User> userList;
+    DatabaseReference databaseReference;
+    AlertDialog dialog;
+    String name, phone;
+    User Admin;
+    ShimmerFrameLayout shimmerFrameLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_admins);
         defineViews();
+        getUsers();
 
+        SearchManager searchManager =
+                (SearchManager) getApplicationContext().getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView =
+                (SearchView) findViewById(R.id.search_bar);
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(this.getComponentName()));
+        searchView.setQueryHint("search");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adminAdapter.filter(query);
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (adminAdapter != null)
+                    adminAdapter.filter(newText);
+                return true;
+            }
+        });
+
+    }
+
+    private void getUsers() {
+        allAdmins_recyclerView.setHasFixedSize(true);
+        allAdmins_recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        userList = new ArrayList<>();
+        adminAdapter = new AdminAdapter(getApplicationContext());
+        adminAdapter.setAdapterCallback(this);
+        try {
+
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        userList.clear();
+                        for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                            User user = postSnapshot.getValue(User.class);
+                            userList.add(user);
+                        }
+
+                        adminAdapter.setUserList(userList);
+                        allAdmins_recyclerView.setAdapter(adminAdapter);
+                        adminAdapter.notifyDataSetChanged();
+                        shimmerFrameLayout.stopShimmer();
+                        shimmerFrameLayout.setVisibility(View.GONE);
+                        allAdmins_recyclerView.setVisibility(View.VISIBLE);
+                    }
+                    else {
+                        Toast.makeText(AllAdminsActivity.this, "NO admins found", Toast.LENGTH_SHORT).show();
+                        shimmerFrameLayout.stopShimmer();
+                        shimmerFrameLayout.setVisibility(View.GONE);
+                        allAdmins_recyclerView.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+
+        }
     }
 
     private void defineViews() {
         back_btn = findViewById(R.id.btn_back);
+        allAdmins_recyclerView = findViewById(R.id.recycler_allAdmins);
+        add_btn = findViewById(R.id.btn_add);
+        shimmerFrameLayout = findViewById(R.id.shimmer_view_library);
+        shimmerFrameLayout.startShimmer();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("Admins");
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,15 +141,60 @@ public class AllAdminsActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        add_btn = findViewById(R.id.btn_add);
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), AddAdminActivity.class));
             }
         });
-        allAdmins_recyclerView = findViewById(R.id.recycler_allAdmins);
+    }
+
+    @Override
+    public void editData(User user) {
+        Admin = user;
+        initDialog();
+    }
+
+
+    public void initDialog() {
+        Button btn_edit;
+        EditText et_name, et_phone;
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("Admins");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_edit_driver, null);
+        et_name = view.findViewById(R.id.et_name);
+        et_phone = view.findViewById(R.id.et_phone);
+        btn_edit = view.findViewById(R.id.btn_edit);
+
+        et_name.setText(Admin.getName());
+        et_phone.setText(Admin.getPhone());
+
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = et_name.getText().toString();
+                phone = et_phone.getText().toString();
+                Admin.setName(name);
+                Admin.setPhone(phone);
+
+                databaseReference.child(Admin.getId()).setValue(Admin).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(AllAdminsActivity.this, "Edited", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+
+
+            }
+        });
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
 
     }
+
 }
---
+++ app/src/main/java/com/goread/library/admin/adapters/AdminAdapter.java
@@ -0,0 +1,124 @@
+package com.goread.library.admin.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.goread.library.R;
+import com.goread.library.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class AdminAdapter extends RecyclerView.Adapter<AdminAdapter.ImageViewHolder> {
+
+    Context mContext;
+    List<User> userList;
+    List<User> tempUserList;
+    private AdapterCallback adapterCallback;
+
+
+    public AdminAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public void setAdapterCallback(AdapterCallback adapterCallback) {
+        this.adapterCallback = adapterCallback;
+    }
+
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.admin_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        User user = userList.get(position);
+
+        holder.username.setText(user.getName());
+        holder.phone.setText(user.getPhone());
+
+
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapterCallback.editData(user);
+            }
+        });
+
+
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+        tempUserList = new ArrayList();
+        tempUserList.addAll(this.userList);
+        notifyDataSetChanged();
+    }
+
+    public static interface AdapterCallback {
+
+
+        void editData(User user);
+
+    }
+
+    public void filter(String charText) {
+
+        charText = charText.toLowerCase(Locale.getDefault());
+        charText = charText.replace("أ", "ا");
+        charText = charText.replace("إ", "ا");
+        charText = charText.replace("آ", "ا");
+        charText = charText.replace("ى", "ي");
+        charText = charText.replace("ئ", "ي");
+        charText = charText.replace("ؤ", "و");
+        charText = charText.replace("ة", "ه");
+
+
+        userList.clear();
+
+        if (charText.length() == 0) {
+            userList.addAll(tempUserList);
+
+        } else {
+            for (User obj : tempUserList) {
+                if (obj.getName().toLowerCase().contains(charText)
+                        || obj.getPhone().contains(charText)) {
+
+                    userList.add(obj);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        TextView username, email, phone;
+        ImageView btnCall, btnEdit;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.tv_driver_name);
+            phone = itemView.findViewById(R.id.tv_phone);
+            btnEdit = itemView.findViewById(R.id.btnEdit);
+
+        }
+    }
+}
