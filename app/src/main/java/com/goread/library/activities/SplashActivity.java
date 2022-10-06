d70a69ff9acc31284ef923d0027edd332e10cb7b
koka_a <malakalsayaghi@gmail.com>
2022-10-06 17:28:14 +0000

    add splash

--- app/src/main/java/com/goread/library/activities/SplashActivity.java
+++ app/src/main/java/com/goread/library/activities/SplashActivity.java
@@ -3,6 +3,9 @@ package com.goread.library.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +17,8 @@ import com.goread.library.libraries.activities.LibraryMainActivity;
 public class SplashActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-
+    Animation anim;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +38,32 @@ public class SplashActivity extends AppCompatActivity {
                 }
                 finish();
             }
-        }, 3000);
+        }, 2000);
+        imageView =findViewById(R.id.imageView); // Declare an imageView to show the animation.
+        ////////////////////
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (mAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(SplashActivity.this, LibraryMainActivity.class));
+                } else {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                }
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(anim);
+        /////////////////////
     }
 
     @Override
